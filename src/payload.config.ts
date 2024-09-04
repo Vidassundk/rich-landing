76d479f92fc4f0676d21ffe68b690b75bdd9a525
vidassundk <vidassundk@gmail.com>
2024-09-04 19:20:23 +0200

    Fixed Vercel deployment typescript issue on payload config

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -194,7 +194,7 @@ export default buildConfig({
           prefix: 'media',
         },
       },
-      bucket: process.env.S3_BUCKET,
+      bucket: process.env.S3_BUCKET as string,
       config: {
         credentials: {
           accessKeyId: process.env.S3_ACCESS_KEY_ID,
